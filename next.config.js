53e71ef96acbf43fb90e13d980be1b68dbbd316c
ansshameed <ansshameed@icloud.com>
2024-08-30 18:50:37 +0100

    deployment changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      RESEND_API_KEY: process.env.RESEND_API_KEY,  
-    },
-  }
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+const nextConfig = {
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
+  basePath: isProd ? '/website-portfolio-2' : '',
+  assetPrefix: isProd ? '/website-portfolio-2/' : '',
+}
+
+module.exports = nextConfig;
